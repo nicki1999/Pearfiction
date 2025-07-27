9024be8a07badf26ef994b9f4a277049e65f4548
Nicki Najafi <nickinajafi1999@gmail.com>
2025-07-26 21:01:08 -0400

    fixed the scoring mechanics

--- src/scripts/game/ScoreLogic.js
+++ src/scripts/game/ScoreLogic.js
@@ -7,17 +7,17 @@ export class ScoreLogic {
   }
 
   checkPaylines() {
-    let winsOnThisSpin = 0;
+    let totalPayout = 0;
     this.gameState.score.winDetail = [];
 
     this.paylines.forEach((line, idx) => {
       const symbols = line.map(([r, c]) => this.gameState.updatedMatrix[r][c]);
-      const win = this.checkPayline(symbols, idx + 1);
-      if (win) winsOnThisSpin++;
+      const linePayout = this.checkPayline(symbols, idx + 1);
+      totalPayout += linePayout;
     });
 
     // Set the total number of wins (not just +1)
-    this.gameState.score.winNumber = winsOnThisSpin;
+    this.gameState.score.winNumber = totalPayout;
 
     // Emit the updated score so LabelScore can display it
     this.eventEmitter.emit("score", this.gameState.score);
@@ -26,6 +26,7 @@ export class ScoreLogic {
   checkPayline(payline, paylineID) {
     const uniqueValues = [...new Set(payline)];
     let winFound = false;
+    let linePayout = 0;
 
     for (const value of uniqueValues) {
       const matches = payline.filter((cell) => cell === value).length;
@@ -33,10 +34,11 @@ export class ScoreLogic {
       if (value !== "" && matches >= 3) {
         const payout = this.payout(value, matches);
         this.addWinDetailToState(paylineID, value, matches, payout);
+        linePayout += payout;
         winFound = true;
       }
     }
-    return winFound;
+    return linePayout;
   }
 
   payout(symbol, matches) {
